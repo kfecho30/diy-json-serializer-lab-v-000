ae955916ab7f77eb0520e93f880a580d88df165f
Kelsey Fecho <kfecho30@gmail.com>
2018-07-02 12:56:27 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,2 +1,14 @@
 class ProductSerializer
+
+  def self.serialize(p)
+    sp = '{'
+    sp += '"id": ' + p.id.to_s + ', '
+    sp += '"name": "' + p.name+ '", '
+    sp += '"price": "' +p.price.to_s+ '",'
+    sp += '"inventory": "' + p.inventory + '",'
+    sp += '"description": "' + p.description
+    sp += '}'
+  end
 end
+
+name, price, inventory and description
